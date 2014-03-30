4718636f5182e98674c8dc8ca7857746a22f8b69
Ben Tomasik <b.tomasik@gmail.com>
2014-03-30 12:24:21 -0500

    Fixed prefix from iostat to inet_dev

--- inet_dev.sh
+++ inet_dev.sh
@@ -15,10 +15,10 @@ tail -n+3 /proc/net/dev | sed 's/^\s\+//g' | egrep -v "^${IGNORE_DEVICES}" | \
 ( while read netstatLine; do \
     netdev=$(echo $netstatLine | cut -d' ' -f1 | sed 's/://g')
     ( \
-        echo "${STAT_PREFIX}.iostat.${netdev}.rx.bytes:$(echo $netstatLine | cut -d' ' -f2)|c" ; \
-		echo "${STAT_PREFIX}.iostat.${netdev}.rx.pkts:$(echo $netstatLine | cut -d' ' -f3)|c" ; \
-		echo "${STAT_PREFIX}.iostat.${netdev}.tx.bytes:$(echo $netstatLine | cut -d' ' -f10)|c" ; \
-		echo "${STAT_PREFIX}.iostat.${netdev}.tx.pkts:$(echo $netstatLine | cut -d' ' -f11)|c" ; \
+        echo "${STAT_PREFIX}.inet_dev.${netdev}.rx.bytes:$(echo $netstatLine | cut -d' ' -f2)|c" ; \
+		echo "${STAT_PREFIX}.inet_dev.${netdev}.rx.pkts:$(echo $netstatLine | cut -d' ' -f3)|c" ; \
+		echo "${STAT_PREFIX}.inet_dev.${netdev}.tx.bytes:$(echo $netstatLine | cut -d' ' -f10)|c" ; \
+		echo "${STAT_PREFIX}.inet_dev.${netdev}.tx.pkts:$(echo $netstatLine | cut -d' ' -f11)|c" ; \
     ) \
 done ) | nc -w 1 -u $STATSD_HOST $STATSD_PORT
 
